3cff34dfbadecdee51268ed4a6d93f1f9fd62945
yeronge <114388258+yeronge@users.noreply.github.com>
2022-12-16 10:33:51 +0900

    commit

---
+++ frontend/src/components/DecreaseInventoryCommand.vue
@@ -0,0 +1,62 @@
+<template>
+
+    <v-card outlined>
+        <v-card-title>
+            DecreaseInventory
+        </v-card-title>
+
+        <v-card-text>
+            <Number label="Qty" v-model="value.qty" :editMode="editMode"/>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="decreaseInventory"
+            >
+                DecreaseInventory
+            </v-btn>
+            
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="close"
+            >
+                Close
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+
+</template>
+
+<script>
+   
+    export default {
+        name: 'DecreaseInventoryCommand',
+        components:{},
+        props: {},
+        data: () => ({
+            editMode: true,
+            value: {},
+        }),
+        created() {
+            this.value.qty = 0;
+        },
+        watch: {
+        },
+        methods: {
+            decreaseInventory() {
+                this.$emit('decreaseInventory', this.value);
+            },
+            close() {
+                this.$emit('closeDialog');
+            },
+            change() {
+                this.$emit('input', this.value);
+            },
+        }
+    }
+</script>
+
---
+++ monolith/src/main/java/labshopmonolith/domain/Order.java
@@ -0,0 +1,83 @@
+package labshopmonolith.domain;
+
+import labshopmonolith.domain.OrderPlaced;
+import labshopmonolith.MonolithApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+
+@Entity
+@Table(name="Order_table")
+@Data
+
+public class Order  {
+
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private String productId;
+    
+    
+    
+    
+    
+    private Integer qty;
+    
+    
+    
+    
+    
+    private String customerId;
+    
+    
+    
+    
+    
+    private Double amount;
+
+    @PostPersist
+    public void onPostPersist(){
+
+        //Following code causes dependency to external APIs
+        // it is NOT A GOOD PRACTICE. instead, Event-Policy mapping is recommended.
+
+        labshopmonolith.external.DecreaseInventoryCommand decreaseInventoryCommand = new labshopmonolith.external.DecreaseInventoryCommand();
+        // mappings goes here
+        MonolithApplication.applicationContext.getBean(labshopmonolith.external.InventoryService.class)
+            .decreaseInventory(/* get???(), */ decreaseInventoryCommand);
+
+
+
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+    }
+    @PrePersist
+    public void onPrePersist(){
+    }
+
+    public static OrderRepository repository(){
+        OrderRepository orderRepository = MonolithApplication.applicationContext.getBean(OrderRepository.class);
+        return orderRepository;
+    }
+
+
+
+
+
+
+}
